29d49bcf9762dcc5cf0450f69aa5d6baa3736031
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-11-06 23:36:26 +0900

    chore: poc using selection change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,51 @@
 <script lang="ts">
-	import { createHandleDelete } from '.';
+	import debounce from 'debounce';
 
-	let editor = $state<HTMLElement>();
-	const handleDelete = $derived(createHandleDelete(editor));
+	let editor: HTMLElement;
+
+	let cursorInPlainText = $state(false);
+	let selectedPlainText = $state(false);
+
+	const handleSelectionChange = () => {
+		cursorInPlainText = false;
+		selectedPlainText = false;
+
+		const selection = window.getSelection();
+		if (!selection || !selection.rangeCount) return;
+
+		const range = selection.getRangeAt(0);
+		const node = range.commonAncestorContainer;
+
+		if (!editor.contains(node)) return;
+		if (node.nodeType !== Node.TEXT_NODE) return;
+
+		// NOTE Click the middle of the editor and then click away from it, blurring the focus.
+		// The selection range is updated as if the invisible cursor has moved to the editor's end.
+		if (range.collapsed && document.activeElement !== editor) return selection.removeAllRanges();
+
+		if (range.collapsed) cursorInPlainText = true;
+		if (!range.collapsed) selectedPlainText = true;
+	};
 </script>
 
+<svelte:document onselectionchange={debounce(handleSelectionChange)} />
+
 <div
-	tabindex="0"
-	role="textbox"
-	aria-multiline="true"
-	bind:this={editor}
 	contenteditable
-	onkeydown={handleDelete}
-	ondragstart={handleDelete}
-	ondrop={handleDelete}
-	oncut={handleDelete}
-	onpaste={handleDelete}
+	bind:this={editor}
+	onblur={handleSelectionChange}
 	class="border-2 border-black p-4"
 >
 	This is editable text with a
-	<break time="3s">3s</break>
+	<break time="3s" data-time="3s"></break>
 	pause in the middle.
 </div>
 
 <style lang="postcss">
 	break {
 		@apply bg-yellow-300;
+	}
+	break::after {
+		content: attr(data-time);
 	}
 </style>
